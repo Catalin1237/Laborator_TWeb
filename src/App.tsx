192c5b5f6a549af2da0495031a79b1a73ed94909
Catalin1237 <catalin.vebs2@gmail.com>
2022-03-23 08:22:07 +0200

    Lab2

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { GreetMe } from './components/GreetMe';
+
+function App() {
+  return (
+    <div className="App">
+      <GreetMe first='Calestru' last='Catalin' group='CR-191' />
+    </div>
+  );
+}
+
+export default App;
