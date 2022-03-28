3879e404b77a24f4bd1375f49d3ff7caf747c3f2
Catalin1237 <catalin.vebs2@gmail.com>
2022-03-28 07:31:18 +0300

    inceput lab 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { MyLayout } from './components/lab3/MyLayout';
 // import { GreetMe } from './components/GreetMe';
-
+import { MyLayout } from './components/lab3/MyLayout';
 
 function App() {
   return (
---
+++ src/components/lab4/superHeroInfo.tsx
@@ -0,0 +1,10 @@
+import SimpleName from "./nameSurname";
+
+interface Hero extends SimpleName {
+    hName: string,
+    power: string,
+    add: string
+}
+
+
+export default Hero;
