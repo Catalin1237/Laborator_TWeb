b6794f1d8424bd6728021b2a10768db42d01c6c6
Catalin1237 <catalin.vebs2@gmail.com>
2022-03-28 08:56:40 +0300

    au fost adaugate 2 interfete cu useState

--- src/components/lab4/Form1.tsx
+++ src/components/lab4/Form1.tsx
@@ -1,7 +1,40 @@
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, notification} from 'antd';
+import { useState } from 'react';
 import SimpleName from './nameSurname';
+import Hero from './superHeroInfo';
 
-export const Form1 = () => {
+
+
+export interface HeroProps{
+    hero: Hero[];
+}
+
+export const FormHero = ({hero}: HeroProps) => {
+
+    const onFinish = () => {
+        notification.open({
+            message: 'Deschide consola pe F12.',
+          });
+    }
+    
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
+    const [hName, sethName] = useState("");
+    const [power, setPower] = useState("");
+    const [add, setAdd] = useState("");
+
+    const submitValue = () =>{
+        const fullInfo ={
+            "1. Name" : name,
+            "2. Surname" : surname,
+            "3. Hero Name" : hName,
+            "4. Power" : power,
+            "5. Address" : add,
+        };
+
+        console.log(fullInfo);
+    }
+
     return(
 
         <Form
@@ -10,23 +43,70 @@ export const Form1 = () => {
             wrapperCol={{ span: 5 }}
             initialValues={{ remember: true }}
             autoComplete="off"
+            onFinish={ onFinish }
         >
             <Form.Item
-            label="Nume"
+            label="Name"
             name="name"
+            rules={[
+                { required: true, message: "Insert Hero real name!"}
+            ]}
             >
-            <Input />
+            <Input 
+                onChange={ (e) => {setName(e.target.value);}}
+            />
             </Form.Item>
     
             <Form.Item
-            label="Prenume"
+            label="Surname"
             name="surname"
+            rules={[
+                { required: true, message: "Insert Hero real surname!"}
+            ]}
             >
-            <Input />
+            <Input 
+                onChange={ (e) => {setSurname(e.target.value);}}
+            />
+            </Form.Item>
+
+            <Form.Item
+            label="Hero name"
+            name="hName"
+            rules={[
+                { required: true, message: "Insert Hero name!"}
+            ]}
+            >
+                <Input 
+                    onChange={ (e) => {sethName(e.target.value);}}
+                />
+            </Form.Item>
+
+            <Form.Item
+            label="Superpower"
+            name="power"
+            rules={[
+                { required: true, message: "Insert Hero powers!"}
+            ]}
+            >
+                <Input 
+                    onChange={ (e) => {setPower(e.target.value);}}
+                />
+            </Form.Item>
+
+            <Form.Item
+            label="Address"
+            name="add"
+            rules={[
+                { required: true, message: "Insert Hero address!"}
+            ]}
+            >
+                <Input 
+                    onChange={ (e) => {setAdd(e.target.value);}}
+                />
             </Form.Item>
     
             <Form.Item wrapperCol={{ offset: 9, span: 7 }}>
-            <Button type="primary" htmlType="submit">
+            <Button type="primary" htmlType="submit" onClick={ submitValue }>
                 Submit
             </Button>
             </Form.Item>
