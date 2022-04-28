a89cc9f5593683f1fd07ab5da99974a4cafe8e24
Catalin1237 <catalin.vebs2@gmail.com>
2022-04-28 10:45:34 +0300

    lab6 login form

--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -2,9 +2,10 @@ import { Layout, Menu } from 'antd';
 import { Routes, Route, Link } from "react-router-dom";
 import { ContentLayout } from './ContentLayout';
 import InfoCard from './InfoCard';
-import InsertAndRead from './InsertAndReadLocal';
-import './layout.css';
+import { InsertAndRead } from './InsertAndReadLocal';
 import TabelInfo from './TableInfo';
+import Login from './LoginForm';
+import './layout.css';
 
 const { Header } = Layout;
 
@@ -15,15 +16,18 @@ export const HeaderLayout = () => {
             <Menu theme='dark' mode='horizontal'>
 
                 <Menu.Item key={1}>
-                    <Link to="/">Insert Info</Link>
+                    <Link to="/">Login</Link>
                 </Menu.Item>
                 <Menu.Item key={2}>
-                    <Link to="/cards">Cards</Link>
+                    <Link to="/insertCard">Insert Info</Link>
                 </Menu.Item>
                 <Menu.Item key={3}>
-                    <Link to="/table">Table info</Link>
+                    <Link to="/cards">Cards</Link>
                 </Menu.Item>
                 <Menu.Item key={4}>
+                    <Link to="/table">Table info</Link>
+                </Menu.Item>
+                <Menu.Item key={5}>
                     <Link to="/insert">Insert localStorage</Link>
                 </Menu.Item>
                 
@@ -31,7 +35,8 @@ export const HeaderLayout = () => {
         </Header>
 
         <Routes>
-            <Route path='/' element={<ContentLayout />} />
+            <Route path='/' element={<Login />} />
+            <Route path='/insertCard' element={<ContentLayout />} />
             <Route path='/cards' element={<InfoCard />} />
             <Route path='/table' element={<TabelInfo />} />
             <Route path='/insert' element={<InsertAndRead />} />
